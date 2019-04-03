138e2516d8bd2c8d4ceb8ddd32a2273f09901d25
dw-hb <42311622+dw-hb@users.noreply.github.com>
2019-04-03 01:49:08 -0400

    More batch stuff

--- eba_ln_install.bat
+++ eba_ln_install.bat
@@ -15,12 +15,16 @@ REM | End of user-adjustable paremetres.
 REM | Begin developer parametres.
 REM | ----------------------------------
 REM | Exit behaviour; Change this to include /b if you want cmd to remain open. Useful for development.
-SET exitCmd=EXIT /b
+SET exitCmd=EXIT
 
 ECHO Requesting for administrative privileges...
 cd /d "%~dp0" && (if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs") && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit )
 ECHO.
 ECHO Privileges received!
+ECHO.
+ECHO Current game directory is %des_ln%.
+ECHO Note: If this is incorrect, terminate this batch script and edit line
+ECHO       12 so %%des_ln%% reflects the cars directory you wish to affect.
 ECHO.
 IF NOT EXIST %~dp0\.installed GOTO eba_ln_init
 IF EXIST %~dp0\.installed GOTO eba_modinst
